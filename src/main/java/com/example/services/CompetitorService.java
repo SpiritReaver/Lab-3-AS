471e08a145be01d06a51f5ab4d8f50cc5c67ead9
SpiritReaver <54217513+SpiritReaver@users.noreply.github.com>
2020-06-03 00:23:29 -0500

    Update CompetitorService.java

--- src/main/java/com/example/services/CompetitorService.java
+++ src/main/java/com/example/services/CompetitorService.java
@@ -5,16 +5,23 @@
  */
 package com.example.services;
 
+import com.example.PersistenceManager;
 import com.example.models.Competitor;
 import com.example.models.CompetitorDTO;
-import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.codehaus.jettison.json.JSONObject;
 
 /**
  *
@@ -24,27 +31,86 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class CompetitorService {
 
+    @PersistenceContext(unitName = "Competitors")
+    EntityManager entityManager;
+
+    @PostConstruct
+    public void init() {
+        try {
+            entityManager = PersistenceManager.getInstance().getEntityManagerFactory().createEntityManager();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @GET
+    @Path("/get")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAll() {
-
-        
-        List<Competitor> competitors = new ArrayList<Competitor>();
-        Competitor competitorTmp= new Competitor("Carlos", "Alvarez", 35, "7658463", "3206574839 ", "carlos.alvarez@gmail.com", "Bogota", "Colombia", false);
-        Competitor competitorTmp2= new Competitor("Gustavo", "Ruiz", 55, "2435231", "3101325467", "gustavo.ruiz@gmail.com", "Buenos Aires", "Argentina", false);
-        competitors.add(competitorTmp);
-        competitors.add(competitorTmp2);
+        Query q = entityManager.createQuery("select u from Competitor u order by u.surname ASC");
+        List<Competitor> competitors = q.getResultList();
         return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(competitors).build();
+
     }
 
     @POST
     @Path("/add")
     @Produces(MediaType.APPLICATION_JSON)
     public Response createCompetitor(CompetitorDTO competitor) {
+        JSONObject rta = new JSONObject();
+        Competitor competitorTmp = new Competitor();
+        competitorTmp.setAddress(competitor.getAddress());
+        competitorTmp.setAge(competitor.getAge());
+        competitorTmp.setCellphone(competitor.getCellphone());
+        competitorTmp.setCity(competitor.getCity());
+        competitorTmp.setCountry(competitor.getCountry());
+        competitorTmp.setName(competitor.getName());
+        competitorTmp.setSurname(competitor.getSurname());
+        competitorTmp.setPassword(competitor.getPassword());
+        competitorTmp.setTelephone(competitor.getTelephone());
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(competitorTmp);
+            entityManager.getTransaction().commit();
+            entityManager.refresh(competitorTmp);
+            rta.put("competitor_id", competitorTmp.getId());
+        } catch (Throwable t) {
+            t.printStackTrace();
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            competitorTmp = null;
+        } finally {
+            entityManager.clear();
+            entityManager.close();
+        }
+        return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(rta).build();
 
-        Competitor competitorTmp= new Competitor(competitor.getName(), competitor.getSurname(), competitor.getAge(), competitor.getTelephone(), competitor.getCellphone(), competitor.getAddress(), competitor.getCity(), competitor.getCountry(), false);
-        return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(competitorTmp).build();
+    }
+
+    @GET
+    @Path("/login/{address}/{password}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response login(@PathParam("address") String address, @PathParam("password") String password) {
+
+        try {
+
+            Query q = entityManager.createQuery("SELECT e FROM Competitor e WHERE e.address = :address AND e.password= :password");
+            q.setParameter("address", address);
+            q.setParameter("password", password);
+
+            List<Competitor> usuario = q.getResultList();
+            if (q.getResultList() != null) {
+                return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(usuario).build();
+
+            }
+
+        } catch (NotAuthorizedException s) {
+            s.getMessage();
+        }
+        return null;
+
     }
 
 }
